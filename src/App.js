04f88a5a6319b57fbaedb4966273b1b87f989229
VeganTortoise <55139588+VeganTortoise@users.noreply.github.com>
2019-09-23 10:29:55 +0200

    changed the app from a function to a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Periodic Table of Elements</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Periodic Table of Elements</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
