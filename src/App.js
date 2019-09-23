0a5f3894969916172938058bd4dff9e6325e018e
VeganTortoise <55139588+VeganTortoise@users.noreply.github.com>
2019-09-23 13:26:13 +0200

    Back to before

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Periodic Table of Elements</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+     <h1>Periodic Table of Elements</h1>
+    </div>
+  );
 }
 
 export default App;
