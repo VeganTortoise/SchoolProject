64d981671cb5734996009f61a239a2ddac1d9d71
VeganTortoise <55139588+VeganTortoise@users.noreply.github.com>
2019-09-25 09:28:52 +0200

    new Files added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>Periodic Table of Elements</h1>
+      <Element />
     </div>
   );
 }
---
+++ src/Element.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Element.css'
+
+export class Element extends React.Component{
+    render() {
+        return(
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default Element
