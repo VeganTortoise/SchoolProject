6d5122acedec69e786adc2500abdbf252c59484b
FeliPu <30833846+FeliPu@users.noreply.github.com>
2019-09-27 11:48:29 +0200

    periodic table transfered to react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Element from "./Element";
+import PeriodicTable from "./PeriodicTable";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Periodic Table of Elements</h1>
-      <Element />
+      <PeriodicTable />
     </div>
   );
 }
--- src/Element.js
+++ src/Element.js
@@ -1,12 +1,10 @@
 import React from "react";
-import './Element.css';
+import "./Element.css";
 
-export class Element extends React.Component{
-    render() {
-        return(
-            <div>Hello World</div>
-        )
-    }
+export class Element extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
 }
 
 export default Element;
