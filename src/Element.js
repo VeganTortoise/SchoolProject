1046b6dea301aa9408a4f396e5c334bc1f077317
VeganTortoise <55139588+VeganTortoise@users.noreply.github.com>
2019-09-27 11:51:11 +0200

    changed Element css

--- src/Element.js
+++ src/Element.js
@@ -1,12 +1,16 @@
 import React from "react";
-import './Element.css';
+import "./Element.css";
 
-export class Element extends React.Component{
-    render() {
-        return(
-            <div>Hello World</div>
-        )
-    }
+export class Element extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="number">1</div>
+        <div className="symbol">H</div>
+        <div className="name">Hydrogen</div>
+      </div>
+    );
+  }
 }
 
 export default Element;
